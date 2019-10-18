690b3901e1258182438d51311a5aa53a176772cc
MladenSalipurovic <50460022+MladenSalipurovic@users.noreply.github.com>
2019-10-18 06:18:52 +0200

    Update User.js

--- src/User.js
+++ src/User.js
@@ -4,10 +4,10 @@ const User = (props) => {
 
 	return (
 
-		<div onSubmit={props.getUserData}>
+		<form onSubmit={props.getUserData}>
 			<input type="text" name="username"/>
 			<button>Search</button>
-		</div>
+		</form>
 	)
 }
 
